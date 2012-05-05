77bb19bad774b9e0ee78b57f356b1a3a123b41cc
Julio Makdisse Saito <saitodisse@gmail.com>
2012-05-05 02:13:40 -0300

    added a submodule

---
+++ PizzaMvcApp/Controllers/IngredientsKOController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PizzaMvcApp.Controllers
+{
+    public class IngredientsKOController : Controller
+    {
+        //
+        // GET: /IngredientsKO/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
